002e52dd58b117329bc93b87377684b7cda1ea67
独醒侠 <270307872@qq.com>
2020-09-10 19:32:05 +0800

    创建各种特性分支

--- cloud-service/src/main/java/cn/dyaoming/outman/cloud/CloudServiceApplication.java
+++ cloud-service/src/main/java/cn/dyaoming/outman/cloud/CloudServiceApplication.java
@@ -5,17 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 
 
-//@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CloudServiceApplication {
