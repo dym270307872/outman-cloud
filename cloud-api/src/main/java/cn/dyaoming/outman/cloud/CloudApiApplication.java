f24b6d1af8555066786dcacb6d02d7e50a80139e
独醒侠 <270307872@qq.com>
2020-11-09 18:58:34 +0800

    提交代码，测试注册中心配置中心

--- cloud-api/src/main/java/cn/dyaoming/outman/cloud/CloudApiApplication.java
+++ cloud-api/src/main/java/cn/dyaoming/outman/cloud/CloudApiApplication.java
@@ -3,6 +3,7 @@ package cn.dyaoming.outman.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,11 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 
-
-//@EnableEurekaClient
-@EnableDiscoveryClient
-@SpringBootApplication
-@EnableFeignClients
+@SpringCloudApplication
 public class CloudApiApplication {
 
     /**
--- cloud-service/src/main/java/cn/dyaoming/outman/cloud/CloudServiceApplication.java
+++ cloud-service/src/main/java/cn/dyaoming/outman/cloud/CloudServiceApplication.java
@@ -1,27 +1,21 @@
 package cn.dyaoming.outman.cloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 
-
-//@EnableEurekaClient
-@EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 public class CloudServiceApplication {
 
     /**
      * 实例化RestTemplate，通过@LoadBalanced注解开启均衡负载能力.
+     *
      * @return restTemplate
      */
     @Bean
@@ -36,7 +30,7 @@ public class CloudServiceApplication {
         SpringApplication.run(CloudServiceApplication.class, args);
 //        ServiceStartInit.initSystemConfig(context);
         //加载业务侦听
-        // SystemStartLoadBusinessConfigure.initSystemConfig(LISTENER_PATH);
+//         SystemStartLoadBusinessConfigure.initSystemConfig(LISTENER_PATH);
     }
 
 }
