cfca661eeeff9efdf4c7925abbe1c1922cff61c9
独醒侠 <270307872@qq.com>
2020-11-10 14:57:28 +0800

    调试配置中心

--- cloud-gateway/src/main/java/cn/dyaoming/outman/cloud/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/cn/dyaoming/outman/cloud/gateway/GatewayApplication.java
@@ -1,16 +1,12 @@
 package cn.dyaoming.outman.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-//@EnableDiscoveryClient
-//@EnableFeignClients
-@SpringBootApplication
+@SpringCloudApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class,args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
--- cloud-service/src/main/java/cn/dyaoming/outman/cloud/controllers/DemoController.java
+++ cloud-service/src/main/java/cn/dyaoming/outman/cloud/controllers/DemoController.java
@@ -1,5 +1,7 @@
 package cn.dyaoming.outman.cloud.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 
 @RestController
+@RefreshScope
 @RequestMapping("/demo")
 public class DemoController {
 
+    @Value("${config.value}")
+    private String configValue;
+
     @RequestMapping(value = "/test", method = RequestMethod.POST)
-    public String test(){
+    public String test() {
         return "随机数：" + new Random().nextInt(100);
+    }
+
+
+    @RequestMapping(value = "/config", method = RequestMethod.GET)
+    public String config() {
+        return "configValue：" + configValue;
     }
 }
